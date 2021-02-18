f1818e52bb1e69591bc64aae20039a1da1663b2f
9tw <gerdotradawijaya@gmail.com>
2021-02-18 14:32:07 +0800

    main

--- live-code.go
+++ live-code.go
@@ -12,9 +12,4 @@ func max(num []int) {
 
 func main() {
   max([]int{1,2,3,4})
-  max([]int{5,6,7,8})
-<<<<<<< HEAD
-=======
-  max([]int{9,10,11,12})
->>>>>>> 91c1d6b20e63e6e9f38c7b6ac2a47cfe32a1aa22
 }
