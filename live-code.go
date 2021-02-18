59f22d1e8ad2d52759c664847d9f7c334f7f4c0d
9tw <gerdotradawijaya@gmail.com>
2021-02-18 10:40:56 +0800

    tambah file live code

---
+++ live-code.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func max(num []int) {
+  total := 0
+  for i := 0; i <= len(num)-1; i++ {
+    total += num[i]
+    fmt.Println(total)
+  }
+}
+
+func main() {
+  max([]int{1,2,3,4})
+}
